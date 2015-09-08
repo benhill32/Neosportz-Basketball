0d9b5a7ba5792382cf783fc093c3e4b2e1337102
benhill32 <ben@neocom.co.nz>
2015-09-08 13:55:33 +1200

    1
    1

--- www/js/mainindex.js
+++ www/js/mainindex.js
@@ -18,7 +18,7 @@ function getMenusch(tx) {
 
 
     var sql = "select Distinct DivisionName,DivisionID,_id from MobileApp_Schedule_Menu where Hide = 0 Group by DivisionName,DivisionID  order by DivisionOrderID";
-     alert(sql);
+   //  alert(sql);
 
     tx.executeSql(sql, [], getMenusch_success);
 }
@@ -27,7 +27,7 @@ function getMenusch(tx) {
 function getMenusch_success(tx, results) {
     $('#busy').hide();
     var len = results.rows.length;
-      alert(len);
+    // alert(len);
     for (var i=0; i<len; i++) {
         var menu = results.rows.item(i);
         $('#schedulemenudiv').append('<li><a href="#" onclick="redirectschedules2(' + menu._id + ')">'+ menu.DivisionName + '</a></li>');
@@ -37,7 +37,7 @@ function getMenusch_success(tx, results) {
 
 function redirectschedules2(ID){
 
-    window.location = "../pages/schedules.html?id=" + ID;
+    window.location = "pages/schedules.html?id=" + ID;
 }
 
 
