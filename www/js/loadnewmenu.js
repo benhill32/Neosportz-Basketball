ffad3836a25f696a11dca56fe37e3a53b11e0a3e
benhill32 <ben@neocom.co.nz>
2015-09-09 13:05:30 +1200

    1
    1

--- www/js/loadnewmenu.js
+++ www/js/loadnewmenu.js
@@ -2,7 +2,7 @@ document.addEventListener("deviceready", onDeviceReadymainmenu, false);
 var schstring = "";
 var resultsstring = "";
 var standstring = "";
-
+var Clubstring = "";
 function onDeviceReadymainmenu() {
     deviceIDfunc = device.uuid;
 
@@ -91,6 +91,47 @@ function getMenustandings_success(tx, results) {
 
 
 
+    db.transaction(getdataclubs, errorCBfunc, successCBfunc);
+
+
+
+
+}
+
+
+function getdataclubs(tx) {
+
+    var sql = "select ID,_id ,name,UpdateDateUTC ,Base64,replace(History, '###$$###', '<br>') as History,replace(Contacts, '###$$###', '<br>') as Contacts,UpdateSecondsUTC,Color from MobileApp_clubs order by name";
+    //alert(sql);
+    tx.executeSql(sql, [], getdataclubs_success);
+}
+
+function getdataclubs_success(tx, results) {
+    $('#busy').hide();
+    var len = results.rows.length;
+//alert(len);
+    for (var i=0; i<len; i++) {
+        var menu = results.rows.item(i);
+        var imgg = "";
+        if(menu.Base64 != "null"){
+            imgg = '<img src="data:image/png;base64,' + menu.Base64 + '"  align="left" height="40">';
+        }
+
+
+        Clubstring+='<li><a href="#">'+ imgg + menu.name + '</a></li>';
+
+
+      // $('#mainmenu').append('<Div class="mainmenuresult" align="left"  >' +
+      //      '<div class="bold size13" style="padding-bottom:7px;"   >' + imgg + menu.name  +
+      //      '<img src="../img/info.png" height="25" align="right" data-toggle="modal" data-target="#basicModalContact" onclick="loadcontacts(' + menu.ID + ')">' +
+       //     '<img src="../img/team.png" onclick="redirectplayer(' + menu.ID + ')"    align="right" height="25">' +
+      //      '</div>' +
+       //     '<div class="size11" data-toggle="modal" data-target="#basicModalclub" onclick="loadhistory(' + menu.ID + ')">' + menu.History.substring(0,200) + '....<span' +
+       //     'data-toggle="modal" class="size11 blue" data-target="#basicModalclub" onclick="loadhistory(' + menu.ID + ')"  >Read More</span></div>' +
+       //     '</Div>');
+    }
+
+
     if(document.getElementById("indexdiv")!=null) {
 
         //$("#menu").show();
@@ -98,17 +139,19 @@ function getMenustandings_success(tx, results) {
 
     $("#menu").show();
 
-        $("#schedulemenudiv").append(schstring);
-        $("#resultmenudiv").append(resultsstring);
-        $("#standingsmenudiv").append(standstring);
+    $("#schedulemenudiv").append(schstring);
+    $("#resultmenudiv").append(resultsstring);
+    $("#standingsmenudiv").append(standstring);
+    $("#clubsmenudiv").append(Clubstring);
     $(function() {
         $('nav#menu').mmenu();
     });
 
 
-
-
 }
+
+
+
 
 
 function redirectstandings(ID){
