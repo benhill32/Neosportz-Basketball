40fa8ce209628dd6eab61dfe2b8d9f7d6112203e
benhill32 <ben@neocom.co.nz>
2015-09-11 12:03:43 +1200

    1
    1

---
+++ www/js/Scoringrequest.js
@@ -0,0 +1,34 @@
+
+document.addEventListener("deviceready", onDeviceReadyscoring, false);
+var deviceIDscore = 0;
+
+
+
+
+function onDeviceReadyscoring() {
+    deviceIDscore = device.uuid;
+
+    $("#lblappid").val(deviceIDscore)
+    db.transaction(getdatascore, errorCBfunc, successCBfunc);
+}
+
+function getdatascore(tx) {
+
+    var sql = "select ID ,name,Fav from MobileApp_clubs order by name";
+    //alert(sql);
+    tx.executeSql(sql, [], getdatascore_success);
+}
+
+function getdatascore_success(tx, results) {
+    $('#busy').hide();
+    var len = results.rows.length;
+//alert(len);
+    var option = '';
+    for (var i = 0; i < len; i++) {
+        var menu = results.rows.item(i);
+        option += '<option value="' + numbers[i] + '">' + numbers[i] + '</option>';
+
+    }
+
+    $('#idclub').append(option);
+}
