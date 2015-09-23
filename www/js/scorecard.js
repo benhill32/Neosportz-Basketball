a8b6b54a3620b38e207e75967b06e232ec992f07
benhill32 <ben@neocom.co.nz>
2015-09-23 12:20:20 +1200

    1
    1

--- www/js/scorecard.js
+++ www/js/scorecard.js
@@ -349,7 +349,9 @@ function savegame(id){
     if(response = "{'Success' : [{'Message': 'Everything is Good'}]"){
         // alert(response);
         //onclicksyncloaddata();
-        onDeviceReadyscore()
+       // onDeviceReadyscore()
+
+        location.reload(true);
       //  db.transaction(getdata, errorCBfunc, successCBfunc);
       //  db.transaction(getqtrs, errorCBfunc, successCBfunc);
     }
