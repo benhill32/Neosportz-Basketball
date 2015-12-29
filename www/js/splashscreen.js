42520926d0f907c5d49e49395b4449271e5c5962
benhill32 <ben@neocom.co.nz>
2015-12-30 11:19:50 +1300

    1
    1

--- www/js/splashscreen.js
+++ www/js/splashscreen.js
@@ -9,13 +9,62 @@ function onDeviceReadysplashscreen() {
 
     db.transaction(getbackground, errorCBfunc, successCBfunc);
 }
-
-
-function getorientsplash(strorein){
-    orientationstand = strorein;
-
-    db.transaction(getbackground, errorCBfunc, successCBfunc);
+function getoneoff2(tx) {
+    var sql = "select oneoffs,token,fliterON,isadmin,allowscore,allowcancel,Clubedit,Ref,Region,allownewfeed,startpage from MobileApp_LastUpdatesec";
+    // alert(sql);
+    tx.executeSql(sql, [], getoneoff2_success);
 }
+
+function getoneoff2_success(tx, results) {
+    $('#busy').hide();
+    var len = results.rows.length;
+
+
+    if(len != 0) {
+        var menu = results.rows.item(0);
+
+
+        window.localStorage.setItem("startpage", menu.startpage);
+        window.localStorage.setItem("allownewfeed", menu.allownewfeed);
+        window.localStorage.setItem("Region", menu.Region);
+        window.localStorage.setItem("apptoken", menu.token);
+        window.localStorage.setItem("fliter", menu.fliterON);
+        window.localStorage.setItem("isadmin", menu.isadmin);
+        window.localStorage.setItem("allowscore", menu.allowscore);
+        window.localStorage.setItem("allowcancel", menu.allowcancel);
+        window.localStorage.setItem("Clubedit", menu.Clubedit);
+        window.localStorage.setItem("Ref", menu.Ref);
+
+
+        if(menu.startpage == 1){
+            splashpage = "../index.html";
+        }else if(menu.startpage == 'null'){
+            splashpage = "../index.html";
+        }else if(menu.startpage == 2){
+            splashpage = "schedules.html";
+        }else if(menu.startpage == 3){
+            splashpage = "standings.html";
+        }else if(menu.startpage == 4){
+            splashpage = "clubs.html";
+        }else if(menu.startpage == 5){
+            splashpage = "news.html";
+        }
+
+    ;
+
+        runadmob();
+
+        window.setTimeout(function(){
+            window.location.href= splashpage;
+        }, 3000);
+
+    }
+}
+
+
+
+
+
 
 
 function getbackground(tx) {
@@ -37,11 +86,9 @@ function getbackground_success(tx, results) {
         Base64 = menu.Base64;
         db.transaction(getbackground2, errorCBfunc, successCBfunc);
     }else{
-        runadmob();
+      //  runadmob();
 
-        window.setTimeout(function(){
-            window.location.href='../index.html';
-        }, 5000);
+        db.transaction(getoneoff2, errorCBfunc, successCBfunc);
     }
 }
 
@@ -57,28 +104,26 @@ function getbackground_success2(tx, results) {
     // alert(len);
     if(len != 0) {
         var menu = results.rows.item(0);
-        $('#splashscreen').empty();
-        $('#splashscreen').append('<img id="screensplashimg" onclick="URLredirect(\'' + menu.URLLINK + '\')" src="data:image/png;base64,' + Base64 + '">');
+      //  $('#splashscreen').empty();
+       // $('#splashscreen').append('<img id="screensplashimg" onclick="URLredirect(\'' + menu.URLLINK + '\')" src="data:image/png;base64,' + Base64 + '">');
 
-        runadmob();
+       // runadmob();
 
 
-        window.setTimeout(function(){
-            window.location.href='../index.html';
-        }, 5000);
-
+        db.transaction(getoneoff2, errorCBfunc, successCBfunc);
 
     }
 }
 
 
+
 function getbackground2_error(err) {
-    $('#splashscreen').empty();
-    $('#splashscreen').append('<img id="screensplashimg"  src="data:image/png;base64,' + Base64 + '">');
+    //  $('#splashscreen').empty();
+    //  $('#splashscreen').append('<img id="screensplashimg"  src="data:image/png;base64,' + Base64 + '">');
     runadmob();
     window.setTimeout(function(){
-        window.location.href='../index.html';
-    }, 5000);
+        window.location.href="../index.html";
+    }, 3000);
 
 }
 
@@ -87,12 +132,11 @@ function getbackground2_error(err) {
 
 function getbackground_error(err) {
     //Alert("Error processing SQL: " + err.code);
-  // alert("error");
+    //  alert("error");
     runadmob();
-  //  alert("after");
     window.setTimeout(function(){
-        window.location.href='../index.html';
-    }, 5000);
+        window.location.href="../index.html";
+    }, 3000);
 
 
 }
